61419ed3fc064c07a88fe2d1beb5b8f1c3f23242
Muhammad Kamran Dilmir <mdilmir@student.unimelb.edu.au>
2016-11-16 09:56:56 +1100

    HTML datable is filled with data from SQLite and the corresponding PDB structure is shown  below the table.

--- varstructure/core.py
+++ varstructure/core.py
@@ -1,155 +1,76 @@
-# -*- coding: utf-8 -*-
-from string import whitespace
-from argparse import ArgumentParser
-from string import whitespace
-import logging
-import os
-import string
-import sys
-import vcf
-#Bioservices Library
-from bioservices import UniProt
-from bioservices import PDB
-from bioservices import *
-#BioPython Library
-from Bio.PDB import *
-#added to work with sequence
-from Bio import SeqIO
-from Bio.Seq import Seq
-#added for data structures
-from collections import defaultdict
-from collections import OrderedDict
-import itertools
-import pandas as pd
+import flask
 import sqlite3
 
-from Bio import SwissProt
-from flask import Flask, render_template
+app = flask.Flask(__name__)
 
-def parse_args():
-    parser = ArgumentParser()
-    parser.add_argument("--vcf", type=str, dest="vcf", help="Input variant file (vcf)", required=True)
-    parser.add_argument("--output", type=str, dest="out", help="Output file (tabular)", required=True)
-    parser.add_argument("--log", type=str, help="Logs progress in specified file, defaults to stdout.")
-    parser.add_argument("-v", "--verbosity", action="count", default=0)
+DB = "PDB_Chain_Uniprot.db"
 
-    args = parser.parse_args()
+### database access
+def db():
+    db_instance = getattr(flask.g, '_database', None)
+    if db_instance is None:
+        db_instance = flask.g._database = sqlite3.connect(DB)
+    return db_instance
 
+def query_db(query, args=(), one=False):
+    cur = db().execute(query, args)
+    rv = cur.fetchall()
+    cur.close()
+    return (rv[0] if rv else None) if one else rv
 
-    if args.verbosity >= 2:
-        print "{} to the power {} equals {}".format(args.v, args.o, answer)
-    elif args.verbosity >= 1:
-        print "{}^{} == {}".format(args.x, args.y, answer)
-
-    return args
-
-def get_hmm():
-    """Get a thought."""
-    return 'hmmm...'
-
-def hmm():
-    """Contemplation..."""
-    print get_hmm()
-    
-app = Flask(__name__)
-
+def close_connection(exception):
+    db_instance = getattr(flask.g, '_database', None)
+    if db_instance is not None:
+        db_instance.close()
 def main():
-    """ Main function."""
-    args = parse_args()
-    if args.log:
-        logfile = args.log
-        logging.basicConfig(filename=logfile, level=logging.DEBUG, \
-            filemode='w', format='%(asctime)s %(message)s', \
-            datefmt='%Y-%m-%d %H:%M:%S')
-    else:
-        logfile = sys.stdout
+    #print("This is hte python code which executes first")
+    '''
+        main entry point
+    '''
+    #if flask.request.method == 'POST':
+        #print("in post")
+        #return process()
+    #else:
+        #return flask.render_template('main.html', form=flask.request.form)
 
-    outputfile = open(args.out, "w")
-    # Output header
-    outputfile.write("chr\tpos\tid\tref\talt\tgene\tfeature\tfeature_type\tconsequence\tswissprotid\tcurrent_ENSP\tuniprotid\tpdbid\tprotein_position\tamino_acid\n")
+@app.route('/')
+def index():
+    '''
+        analysis
+    '''
+    result = []
+    con = sqlite3.connect("PDB_Chain_Uniprot.db")
+    cur = con.cursor()
+    cur.execute("SELECT a.Chrom,a.POS,a.Vep_ID,a.REF,a.ALLELE,a.Consequence,a.Protein_position, a.Amino_acids, \
+                b.PDB,b.PDB_BEG,b.PDB_END,a.SIFT, a.PolyPhen,b.CHAIN,b.SP_Primary,b.Swissprot_id,max(b.Res_LEN) as RES_Length \
+                FROM Input_VCF_CSQ AS a LEFT JOIN  PDB_Chain_Uniprot AS b ON a.SWISSPROT = b.Swissprot_Id \
+                WHERE a.CANONICAL ='YES' AND Consequence= 'missense_variant' GROUP BY b.Swissprot_Id LIMIT 200;")
+    matches= cur.fetchall()
+    #matches = query_db(("SELECT * FROM Input_VCF_CSQ LIMIT 10"),"",one=True)
+    errors = []
+    #find matching genes
+    #matches = query_db(("SELECT * FROM Input_VCF_CSQ AS a JOIN PDB_Chain_Uniprot AS b ON a.SWISSPROT=b.Swissprot_Id WHERE Consequence IS NOT NULL "),"",one=False)
+    #print(matches)
+    for match in matches:
+    #if matches[1] is not None:
+        print match
+        result.append({'Chrom': str(match[0]), 'POS': str(match[1]), 'Id': str(match[2]), 'REF': str(match[3]),'ALLELE': str(match[4]),'Consequence': str(match[5]),
+        'Protein_position': str(match[6]),'Amino_acids': str(match[7]),'PDB': str(match[8]),
+        'PDB_BEG': str(match[9]),'PDB_END': str(match[10]),
+        'SIFT': str(match[11])[str(match[11]).find("(") + 1:str(match[11]).find(")")],
+        'SIFT_Description': str(match[11])[0:str(match[11]).find("(")],
+        'PolyPhen': str(match[12])[str(match[12]).find("(") + 1:str(match[12]).find(")")],
+        'PolyPhen_Description': str(match[12])[0:str(match[12]).find("(")],'CHAIN': str(match[13]),
+        'SP_Primary': str(match[14]),'RES_Length': str(match[16])})
+    #else:
+        # gene is no good
+        #warnings.append( 'Gene "{}" had no matches'.format(fields[0]))
 
-    vcf_row = {}
-
-    #Interface to the UniProt service
-    u = UniProt(verbose=False)
-    vcf_reader = vcf.Reader(open(args.vcf, 'r'))
-    # writing in a csv file
-    for record in vcf_reader:
-        current_chr = record.CHROM
-        current_id = record.ID
-        current_pos = record.POS
-        current_ref = record.REF
-        current_alt = ','.join(str(v) for v in record.ALT)
-
-        # VEP fields
-        current_gene, current_feature = '',''
-        current_feature_type, current_consequence = '',''
-        current_swissprot,current_ENSP, current_protein, current_pdbid , current_protein_position, current_amino_acid = '','','','','',''
-        if "CSQ" in record.INFO:
-            csq = record.INFO['CSQ']
-            # BELOW: THERE ARE A COUPLE OF OPTIONS TO PROCEED
-            # For going through annotations for all transcript
-            for current_csq_element in csq:
-                current_csq = current_csq_element.split('|')
-                current_consequence = current_csq[1]
-                current_gene = current_csq[4]
-                current_feature_type = current_csq[5]
-                current_feature = current_csq[6]
-                current_protein_position = current_csq[14]
-                current_amino_acid = current_csq[15]
-                current_ENSP = current_csq[26]
-                current_swissprot = current_csq[27]
-                # only cosider missense mutation
-                con = sqlite3.connect("PDB_Chain_Uniprot.db")
-                cur = con.cursor()
-                #print(current_swissprot)
-
-                if current_swissprot != "":
-                    cur.execute("SELECT PDB,SP_PRIMARY FROM PDB_Chain_Uniprot WHERE Swissprot_Id =?",(str(current_swissprot),))
-                    rows = cur.fetchall()
-                    #print(rows)
-                    for row in rows:
-                        #print(row)
-                        current_pdbid = row[0]
-                        #print(row[0])
-                        current_protein = row[1]
-                        #print(row[1])
-                        #print(current_pdbid)
-                        #print(current_protein)
-                        break;
-                            #out_str = [ current_chr, str(current_pos), str(current_id), current_ref, current_alt,current_gene, current_feature, current_feature_type, current_consequence,current_swissport,current_ENSP, current_protein, str(current_pdbid) , current_protein_position, current_amino_acid]
-                else:
-                    current_protein = ""
-                    current_pdbid = ""
-
-                out_str = [ current_chr, str(current_pos), str(current_id), current_ref, current_alt,current_gene, current_feature, current_feature_type, current_consequence,current_swissprot,current_ENSP, current_protein, current_pdbid , current_protein_position, current_amino_acid]
-                out_str = [x or 'None' for x in out_str]
-                outputfile.write("\t".join(out_str))
-                outputfile.write("\n")
-                con.commit()
-                con.close()
-        else:
-            current_gene, current_feature = '',''
-            current_feature_type, current_consequence = '',''
-            current_swissprot,current_ENSP, current_protein, current_pdbid , current_protein_position, current_amino_acid = '','','','','',''
-
-            out_str = [ current_chr, str(current_pos), str(current_id), current_ref, current_alt,
-                        current_gene, current_feature, current_feature_type, current_consequence,current_swissprot,current_ENSP, current_protein, current_pdbid , current_protein_position, current_amino_acid]
-            out_str = [x or 'None' for x in out_str]
-            outputfile.write("\t".join(out_str))
-            outputfile.write("\n")
-
-    outputfile.close()
-    logging.info('Start.')
-    logging.info('Command line: {}'.format(' '.join(sys.argv)))
-
-
-@app.route("/")
-def flskmain():
-    return render_template('index0.html')
-
-    # logging.info()
+    #if len(errors) == 0:
+    return flask.render_template('index.html',result=result)
 
 if __name__ == '__main__':
+    #app.run(debug=False,host='127.0.0.0')
+    #app.run()
     main()
     app.run()
--- varstructure/inputVcfToSqlite.py
+++ varstructure/inputVcfToSqlite.py
@@ -40,106 +40,64 @@ def main():
     cur = con.cursor()
 
     cur.execute("DROP TABLE Input_VCF_CSQ;") # remove the table if it already Exist
-    cur.execute("DROP TABLE Input_VCF_ANN ;") # remove the table if it already Exist
 
     #VEP Annotation
     cur.execute("CREATE TABLE Input_VCF_CSQ (Chrom,POS,Vep_Id,REF,ALT_Full,QUAL,FILTER);")
-    #SNPeff Annotation
-    cur.execute("CREATE TABLE Input_VCF_ANN (Chrom,POS,Vep_Id,REF,ALT_Full,QUAL,FILTER);")
 
     vcf_row = {}
     vcf_reader = vcf.Reader(open(args.vcf, 'r'))
 
-    #my_parser = VCFParser(infile=args.vcf, split_variants=True, check_info=True)
-    #my_parser = VCFParser(infile=args.vcf, split_variants=True, check_info=True)
-    #my_parser = VCFParser(infile=args.vcf)
-    #for line in my_parser.metadata.print_header():
-        #if "CSQ" in line['info_dict']:
-            #csq = line['info_dict']['CSQ']
-            #print(csq)
-        #print(line)
-    #for variant in my_parser:
-        #print('\t'.join(([variant[head] for head in my_parser.header])))
-
-    #for variant in my_parser:
-        #print(variant)
-        #print(variant['CHROM'])
-        #print(variant['ALT'])
-        #print(variant['info_dict']['CSQ'][2])
-        #break
-
+    # CSQ
     len_csq_remove = vcf_reader.infos['CSQ'].desc.find(":")
     csq_header = vcf_reader.infos['CSQ'].desc[0:len_csq_remove+2]
+    current_csq_header = vcf_reader.infos['CSQ'].desc.replace(csq_header,"").split('|')
+    print(current_csq_header)
 
-    for current_csq_header in vcf_reader.infos['CSQ'].desc.replace(csq_header,"").split('|'):
-        print(current_csq_header)
-        cur.execute('ALTER TABLE Input_VCF_CSQ ADD COLUMN ' + str(current_csq_header))
-
-    len_ann_remove = vcf_reader.infos['ANN'].desc.find(":")
-    ann_header = vcf_reader.infos['ANN'].desc[0:len_ann_remove+2]
-
-    for current_ann_header in vcf_reader.infos['ANN'].desc.replace(ann_header,"").split('|'):
-        sub_header =False
-        #print(current_ann_header)
-        for current_ann_sub_header in current_ann_header.split("/"):
-            #print(current_ann_sub_header)
-            cur.execute('ALTER TABLE Input_VCF_ANN ADD COLUMN ' + str(current_ann_sub_header.replace("'","").replace(".","_")))
-            sub_header =True
-        if sub_header == False:
-            cur.execute('ALTER TABLE Input_VCF_ANN ADD COLUMN ' + str(current_ann_header.replace("'","").replace(".","_")))
-
-    con.commit()
+    for current_csq_sub_header in current_csq_header:
+        print(current_csq_sub_header)
+        cur.execute('ALTER TABLE Input_VCF_CSQ ADD COLUMN ' + str(current_csq_sub_header))
+        con.commit()
 
     for record in vcf_reader:
         #','.join(str(v) for v in record.ALT)
-        to_db_master = [(record.CHROM, record.POS,record.ID,record.REF,record.ALT,record.QUAL,record.FILTER)]
+        to_db_master = [str(record.CHROM), str(record.POS),str(record.ID),str(record.REF),"",str(record.QUAL),""]
 
         if "CSQ" in record.INFO:
             csq = record.INFO['CSQ']
+            current_csq_sub_element=[]
             # For going through annotations for all transcript
             for current_csq_element in csq:
                 #current_csq = current_csq_element.split('|')
-                current_csq = current_csq_element.replace("|",",")
-                cur.executemany("INSERT INTO Input_VCF_CSQ
-                            VALUES (?,?,?,?,?,?,?,?,?,?,\
-                                    ?,?,?,?,?,?,?,?,?,?,\
-                                    ?,?,?,?,?,?,?,?,?,?,\
-                                    ?,?,?,?,?,?,?,?,?,?,?);",\
-                                    to_db_master,current_csq )
+                current_csq_sub_element = current_csq_element.split("|")
+
+                print(to_db_master)
+
+                #VALUES(?,?,?,?,?,?,?)",to_db_master)
+                cur.executemany("INSERT INTO Input_VCF_CSQ \
+                                 VALUES (?,?,?,?,?,?,?,?,?,?,\
+                                        ?,?,?,?,?,?,?,?,?,?,\
+                                        ?,?,?,?,?,?,?,?,?,?,\
+                                        ?,?,?,?,?,?,?,?,?,?,\
+                                        ?,?,?,?,?,?,?,?,?,?,?)",\
+                                        ((to_db_master + current_csq_sub_element),))
                 con.commit()
-
-        else if "ANN" in record.INFO:
-            ann = record.INFO['ANN']
-            # For going through annotations for all transcript
-            for current_ann_element in ann:
-                current_ann = current_ann_element.split('|')
-
-                for current_ann_sub_child in current_ann.split("/"):
-                    #myList = ['a','b','c','d']
-                    #myString = ",".join(myList )
-                    to_db_ann_child = ','.join(map(current_ann_sub_child, to_db_ann_child))
-
-
-                    cur.executemany("INSERT INTO Input_VCF_CSQ
-                            VALUES (?,?,?,?,?,?,?,?,?,?,\
-                                    ?,?,?,?,?,?,?,?,?,?,\
-                                    ?,?,?,?,?,?,?,?,?,?,\
-                                    ?,?,?,?,?,?,?,?,?,?,?);",\
-                                    to_db_master,to_db_ann_child )
-            con.commit()
-
         else :
-            to_db_child =[("","","","","","","","","","",\
+            to_db_csq_child =["","","","","","","","","","",\
                        "","","","","","","","","","",\
                        "","","","","","","","","","",\
-                       "","","")]
-        cur.executemany("INSERT INTO Input_VCF_CSQ
-                VALUES (?,?,?,?,?,?,?,?,?,?,\
-                        ?,?,?,?,?,?,?,?,?,?,\
-                        ?,?,?,?,?,?,?,?,?,?,\
-                        ?,?,?,?,?,?,?,?,?,?,?);",\
-                        to_db_master,to_db_child )
-        con.commit()
+                       "","","","","","","","","","",\
+                       "","","",""]
+            print(to_db_master)
+            print(to_db_csq_child)
+
+            cur.executemany("INSERT INTO Input_VCF_CSQ \
+                             VALUES (?,?,?,?,?,?,?,?,?,?,\
+                                    ?,?,?,?,?,?,?,?,?,?,\
+                                    ?,?,?,?,?,?,?,?,?,?,\
+                                    ?,?,?,?,?,?,?,?,?,?,\
+                                    ?,?,?,?,?,?,?,?,?,?,?)",\
+                                    ((to_db_master + to_db_csq_child),))
+            con.commit()
 
     con.close()
 
